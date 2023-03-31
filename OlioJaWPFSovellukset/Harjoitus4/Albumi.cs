32de6ba9d3016df43e3218e15806f8b0b06a5c08
Jere Kekkinen <jere.kekkinen@student.careeria.fi>
2023-03-31 14:11:56 +0300

    Harjoitus 4 tehty.

---
+++ OlioJaWPFSovellukset/Harjoitus4/Albumi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus4
+{
+    internal class Albumi
+    {
+        public string artisti;
+        public string nimi;
+        public string genre;
+        public int hinta;
+
+        public List<string> kappale = new List<string>();
+
+        public void KappaleenLis(string kappaleet)
+        {
+            kappale.Add(kappaleet);
+        }
+        public void TulostaData()
+        {
+            Console.WriteLine("Albumi: ");
+            Console.WriteLine("artisti: " + artisti);
+            Console.WriteLine("nimi: " + nimi);
+            Console.WriteLine("genre: " + genre);
+            Console.WriteLine("hinta: " + hinta + "euroa");
+            Console.WriteLine();
+        }
+
+        public void MusiikkiTulostus()
+        {
+            Console.WriteLine("\n* " + kappale[0]);
+            Console.WriteLine("* " + kappale[1]);
+            Console.WriteLine("* " + kappale[2]);
+            Console.WriteLine("* " + kappale[3]);
+            Console.WriteLine("* " + kappale[4]);
+            Console.WriteLine("* " + kappale[5]);
+        }
+
+    }
+}
---
+++ OlioJaWPFSovellukset/Harjoitus4/Program.cs
@@ -0,0 +1,26 @@
+﻿using Harjoitus4;
+using Harjoitus4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Albumi alb = new Albumi();
+
+        alb.artisti = "DJ Gyrotta Zao";
+        alb.nimi = "LARD";
+        alb.genre = "Hardstyle";
+        alb.hinta = 3;
+
+
+        alb.KappaleenLis("☣ ZETRAKORE XAOS DA JUMPING GARGOYLZ ☣ مسدسات الطبيعة 1.6");
+        alb.KappaleenLis("HelalaBoys[2009.by.RaRMANRiPZz z( 0 _ -@ )....]");
+        alb.KappaleenLis("VALERIA BOUNZE");
+        alb.KappaleenLis("▯▯▯▯");
+        alb.KappaleenLis("卍");
+        alb.KappaleenLis("GREEN SUN BRAIN SICK");
+
+        alb.TulostaData();
+        alb.MusiikkiTulostus();
+    }
+}
