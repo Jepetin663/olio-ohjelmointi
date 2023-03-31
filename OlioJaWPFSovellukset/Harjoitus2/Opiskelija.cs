db038513b7e7e88dcd2bfcd5c852481376ab3e4f
Jere Kekkinen <jere.kekkinen@student.careeria.fi>
2023-03-31 10:49:42 +0300

    Harjoitus 2 on tehty.

---
+++ OlioJaWPFSovellukset/Harjoitus2/Opiskelija.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus2
+{
+    class Opiskelija
+    {
+        private string Nimi { get; set; }
+        private string OpiskelijaID { get; set; }
+        private int Opintopisteet { get; set; }
+
+        public Opiskelija(string nimi, string id, int op)
+        {
+            Nimi = nimi;
+            OpiskelijaID = id;
+            Opintopisteet = op;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Opiskelija: ");
+            Console.WriteLine("Nimi: " + Nimi);
+            Console.WriteLine("Opiskelijan ID: " + OpiskelijaID);
+            Console.WriteLine("Opintopisteet: " + Opintopisteet);
+            Console.WriteLine("");
+        }
+
+        public void MuokkaaOpintopisteitä(int i)
+        {
+            Opintopisteet += i;
+
+            if (Opintopisteet < 0)
+            {
+                Opintopisteet = 0;
+            }
+        }
+    }
+}
--- OlioJaWPFSovellukset/Harjoitus2/Program.cs
+++ OlioJaWPFSovellukset/Harjoitus2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Opiskelija opiskelija1 = new Opiskelija("Jere", "DATV22", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}
